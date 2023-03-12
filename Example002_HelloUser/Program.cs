4dabd3e56bca3068b76456fef7db77535c8b0dc6
Filippov2022 <98344197+Filippov2022@users.noreply.github.com>
2023-03-12 18:40:33 +0300

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿//Hello User WriteLine-новая строка, Write-в одной строке
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
