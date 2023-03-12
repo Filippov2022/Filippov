1fda9e5547d8d9132b8823bb55ec9a423be1bb60
Filippov2022 <98344197+Filippov2022@users.noreply.github.com>
2023-03-12 20:00:30 +0300

    add Example 005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+  Console.WriteLine("Ура! Этоже Маша!");
+}
+else
+{
+  Console.Write("Приыет, ");
+  Console.WriteLine(username);
+}
